24fa1655bdd27a7b3f1edf708fe104b411520619
dangcntt19 <dangzip6@mail.com>
2021-11-19 22:35:24 +0700

    bỏ 1 lable

--- GoogleTranslate/Form1.Designer.cs
+++ GoogleTranslate/Form1.Designer.cs
@@ -41,7 +41,6 @@ namespace GoogleTranslate
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -86,7 +85,7 @@ namespace GoogleTranslate
             this.label3.Location = new System.Drawing.Point(243, 9);
             this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(354, 36);
+            this.label3.Size = new System.Drawing.Size(422, 40);
             this.label3.TabIndex = 6;
             this.label3.Text = "GOOGLE TRANSLATE ";
             // 
@@ -113,7 +112,7 @@ namespace GoogleTranslate
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(559, 65);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(166, 25);
+            this.comboBox1.Size = new System.Drawing.Size(166, 28);
             this.comboBox1.TabIndex = 9;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -122,7 +121,7 @@ namespace GoogleTranslate
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(120, 65);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(166, 25);
+            this.comboBox2.Size = new System.Drawing.Size(166, 28);
             this.comboBox2.TabIndex = 10;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
@@ -136,16 +135,9 @@ namespace GoogleTranslate
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.Size = new System.Drawing.Size(81, 20);
             this.label1.TabIndex = 12;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 6);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 17);
-            this.label2.TabIndex = 13;
+            this.label1.Text = "khánh nè";
             // 
             // button3
             // 
@@ -159,11 +151,10 @@ namespace GoogleTranslate
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(816, 455);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
@@ -196,7 +187,6 @@ namespace GoogleTranslate
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
     }
 }
